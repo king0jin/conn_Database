4ddce8899e9ed146f4d260daac7e13872246c965
king0jin <et0709@naver.com>
2024-11-01 13:17:56 +0900

    데이터 삽입 추가2

--- conn_mongo.py
+++ conn_mongo.py
@@ -15,7 +15,10 @@ try:
     #doc1 = {'empno':'10001', 'name':'lim', 'phone':'011-0000-0000', 'addr':'busan'}
     #collect.insert_one(doc1)
 
-    
+    #데이터 삽입3
+    doc2 = {'empno':'10002', 'name':'kim', 'phone':'012-0000-0000', 'addr':'masan'}
+    doc3 = {'empno':'10003', 'name':'jim', 'phone':'013-0000-0000', 'addr':'jeju'}
+    collect.insert_many([doc2, doc3])
 
     # 데이터 조회
     result = collect.find()
